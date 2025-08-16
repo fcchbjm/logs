c234fc6b7bc481d7b138ed8eff63362cd216fac7
fcchbjm <2748195351@qq.com>
2025-08-16 21:30:05 +0800

    日志落地类-接口的设计

---
+++ sink.hpp
@@ -0,0 +1,81 @@
+#ifndef __LOGS_SINK_H__
+#define __LOGS_SINK_H__
+
+/*
+ * sink.hpp 日志落地模块类
+ * 1. 抽象落地基类
+ * 2. 派生子类（根据不同的落地方向进行派生）
+ * 3. 使用工厂模式进行创建与表示的分离
+*/
+
+#include "util.hpp"
+#include <fstream>
+#include <memory>
+
+namespace cpplogs
+{
+    class LogSink
+    {
+    public:
+        using ptr = std::shared_ptr<LogSink>;
+        virtual ~LogSink() {};
+        virtual void log(const char* data, size_t len) = 0;
+    };
+
+    //落地方向: 标准输出
+    class StdoutSink : public LogSink
+    {
+    public:
+        //将日志消息写入到标准输出
+        void log(const char* data, size_t len)
+        {
+
+        }
+    };
+    //落地方向: 指定文件
+    class FileSink : public LogSink
+    {
+    public:
+        //构造时传入文件名，打开文件，将文件句柄管理起来
+        FileSink(const std::string& pathname)
+        {
+
+        }
+        //将日志消息写入到指定文件
+        void log(const char* data, size_t len)
+        {
+
+        }
+    private:
+        const std::string _pathname;
+        std::ofstream _ofs;
+    };
+    //落地方向: 滚动文件(大小)
+    class RollSinkBySize : public LogSink
+    {
+    public:
+        //构造时传入文件名，打开文件，将文件句柄管理起来
+        RollSinkBySize(const std::string& basename, size_t max_size);
+
+        //将日志消息写入到指定文件
+        void log(const char* data, size_t len);
+
+    private:
+        void createNewFile();//进行大小判断，超过最大大小创建新文件
+    private:
+        //通过基础文件名+扩展文件名（以时间生成）组成一个实际的当前输出文件名
+        std::string _basename;//基础文件名
+        std::ofstream _ofs;
+        size_t _max_fsize;//记录最大大小，当前文件写入大小超过了这个大小就要切换文件
+        size_t _cur_fsize;//记录当前文件已经写入的数据大小
+    };
+    //落地方向: 滚动文件(时间)
+
+    //简单工厂模式
+    class SinkFactory
+    {
+
+    };
+}
+
+#endif

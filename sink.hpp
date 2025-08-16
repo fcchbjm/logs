dc3eeb50764dd7d6f1d07a70ea027eea7f0d01fc
fcchbjm <2748195351@qq.com>
2025-08-16 23:07:54 +0800

    日志落地类-接口的实现

--- sink.hpp
+++ sink.hpp
@@ -11,6 +11,8 @@
 #include "util.hpp"
 #include <fstream>
 #include <memory>
+#include <cassert>
+#include <sstream>
 
 namespace cpplogs
 {
@@ -29,7 +31,7 @@ namespace cpplogs
         //将日志消息写入到标准输出
         void log(const char* data, size_t len)
         {
-
+            std::cout.write(data, len);
         }
     };
     //落地方向: 指定文件
@@ -38,13 +40,19 @@ namespace cpplogs
     public:
         //构造时传入文件名，打开文件，将文件句柄管理起来
         FileSink(const std::string& pathname)
+        : _pathname(pathname)
         {
-
+            //创建日志文件所在的目录
+            cpplogs::util::File::createDirectory(cpplogs::util::File::path(_pathname));
+            //创建并打开日志文件
+            _ofs.open(_pathname, std::ios::binary | std::ios::app);//写入和追加
+            assert(_ofs.is_open());
         }
         //将日志消息写入到指定文件
         void log(const char* data, size_t len)
         {
-
+            _ofs.write(data, len);
+            assert(_ofs.good());
         }
     private:
         const std::string _pathname;
@@ -55,13 +63,56 @@ namespace cpplogs
     {
     public:
         //构造时传入文件名，打开文件，将文件句柄管理起来
-        RollSinkBySize(const std::string& basename, size_t max_size);
+        RollSinkBySize(const std::string& basename, size_t max_size)
+        : _basename(basename)
+        , _max_fsize(max_size)
+        , _cur_fsize(0)
+        {
+            std::string pathname = createNewFile();
+            //创建日志文件所在的目录
+            cpplogs::util::File::createDirectory(cpplogs::util::File::path(pathname));
+            //创建并打开日志文件
+            _ofs.open(pathname, std::ios::binary | std::ios::app);//写入和追加
+            assert(_ofs.is_open());
+        }
 
         //将日志消息写入到指定文件
-        void log(const char* data, size_t len);
+        void log(const char* data, size_t len)
+        {
+            if(_cur_fsize >= _max_fsize)//进行大小判断，超过最大大小创建新文件
+            {
+                std::string pathname = createNewFile();
+                _ofs.close();//关闭原来打开的文件
+                _ofs.open(pathname, std::ios::binary | std::ios::app);//写入和追加
+                assert(_ofs.is_open());
+            }
+            _ofs.write(data, len);
+            _cur_fsize += len;
+            assert(_ofs.good());
+        }
 
     private:
-        void createNewFile();//进行大小判断，超过最大大小创建新文件
+        std::string createNewFile()
+        {
+            //获取系统时间，以时间来构造文件名拓展名
+            time_t cur_time = cpplogs::util::Date::getTime();
+            struct tm st;
+            localtime_r(&cur_time, &st);
+            std::stringstream filename;
+            if(!_basename.empty())
+            {
+                filename << _basename;
+                filename << "-";
+            }
+            filename << st.tm_year;
+            filename << st.tm_mon;
+            filename << st.tm_mday;
+            filename << st.tm_hour;
+            filename << st.tm_min;
+            filename << st.tm_sec;
+            filename << ".log";
+            return filename.str();
+        }
     private:
         //通过基础文件名+扩展文件名（以时间生成）组成一个实际的当前输出文件名
         std::string _basename;//基础文件名

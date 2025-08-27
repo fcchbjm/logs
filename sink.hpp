a401ac8a74af67d44518ea3f95f27cbae874536c
fcchbjm <2748195351@qq.com>
2025-08-27 10:50:49 +0800

    日志落地类-拓展：按照时间滚动文件

--- sink.hpp
+++ sink.hpp
@@ -101,11 +101,7 @@ namespace cpplogs
             struct tm st;
             localtime_r(&cur_time, &st);
             std::stringstream filename;
-            if(!_basename.empty())
-            {
-                filename << _basename;
-                filename << "-";
-            }
+            filename << _basename;
             filename << st.tm_year + 1900;
             filename << st.tm_mon + 1;
             filename << st.tm_mday;
@@ -124,9 +120,95 @@ namespace cpplogs
         size_t _cur_fsize;//记录当前文件已经写入的数据大小
         size_t _name_count;//名称计数器
     };
-    //落地方向: 滚动文件(时间)
 
-    //简单工厂模式
+    enum class TimeGap
+    {
+        GAP_SECOND,
+        GAP_MINUTE,
+        GAP_HOUR,
+        GAP_DAY
+    };
+
+    //落地方向: 滚动文件(时间)
+    class RollSinkByTime : public LogSink
+    {
+    public:
+        //构造时传入文件名，打开文件，将文件句柄管理起来
+        RollSinkByTime(const std::string& basename, cpplogs::TimeGap gap_type)
+        : _basename(basename)
+        {
+            switch (gap_type)
+            {
+            case cpplogs::TimeGap::GAP_SECOND:
+                _gap_size = 1;
+                break;
+            
+            case cpplogs::TimeGap::GAP_MINUTE:
+                _gap_size = 60;
+                break;
+            
+            case cpplogs::TimeGap::GAP_HOUR:
+                _gap_size = 3600;
+                break;
+
+            case cpplogs::TimeGap::GAP_DAY:
+                _gap_size = 24 * 3600;
+                break;
+            
+            default:
+                break;
+            }
+
+            _cur_gap = cpplogs::util::Date::getTime()  % _gap_size;//当前是第几个时间段
+
+            std::string pathname = createNewFile();
+            cpplogs::util::File::createDirectory(cpplogs::util::File::path(pathname));
+            _ofs.open(pathname, std::ios::binary | std::ios::app);//写入和追加
+            assert(_ofs.is_open());
+        }
+
+        //将日志消息写入到指定文件，进行时间判断，超过最大时间间隔则创建新文件
+        void log(const char* data, size_t len)
+        {
+            time_t cur = cpplogs::util::Date::getTime();
+            if(cur % _gap_size > _cur_gap)
+            {
+                std::string pathname = createNewFile();
+                _ofs.close();//关闭原来打开的文件
+                _ofs.open(pathname, std::ios::binary | std::ios::app);//写入和追加
+                assert(_ofs.is_open());
+            }
+            _ofs.write(data, len);
+            assert(_ofs.good());
+        }
+
+    private:
+        std::string createNewFile()
+        {
+            //获取系统时间，以时间来构造文件名拓展名
+            time_t cur_time = cpplogs::util::Date::getTime();
+            struct tm st;
+            localtime_r(&cur_time, &st);
+            std::stringstream filename;
+            filename << _basename;
+            filename << st.tm_year + 1900;
+            filename << st.tm_mon + 1;
+            filename << st.tm_mday;
+            filename << st.tm_hour;
+            filename << st.tm_min;
+            filename << st.tm_sec;
+            filename << ".log";
+            return filename.str();
+        }
+    private:
+        //通过基础文件名+扩展文件名（以时间生成）组成一个实际的当前输出文件名
+        std::string _basename;//基础文件名
+        std::ofstream _ofs;
+        size_t _cur_gap; //当前是第几个时间段
+        size_t _gap_size; //时间段的大小
+    };
+
+    //简单工厂模式 - C++不定参宏函数
     class SinkFactory
     {
     public:
